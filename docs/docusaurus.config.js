e3631c9f24fcdff787bc7462a395dcc2bee6151a
m3rashid <m3rashid.hussain@gmail.com>
2023-05-02 01:57:52 +0530

    init(docs): init docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -125,6 +125,11 @@ const config = {
 				theme: lightCodeTheme,
 				darkTheme: darkCodeTheme,
 			},
+			colorMode: {
+				defaultMode: 'dark',
+				disableSwitch: false,
+				respectPrefersColorScheme: true,
+			},
 		}),
 };
 
